7daa3e11dfa44b00cb3b4ab8fed58ff73eca06cc
Lehan <haaslewer2@gmail.com>
2022-05-18 15:46:31 +0200

    Tidy up

--- amd/src/siyavulaqt.js
+++ amd/src/siyavulaqt.js
@@ -1,30 +1,24 @@
 export const init = (chktrue, chkfalse) => {
-  console.log(chktrue);
-  console.log(chkfalse);
-
-  //a^14
-
   var timer = window.setInterval(() => {
-    var height = $("#siyavulaQContainer")
+    var height = $(".question-content")
       .contents()
       .find(".sv-region-main")
       .height();
 
-    var qtvalue = $("#siyavulaQContainer").contents().find(".sv-form__actions");
+    var qtvalue = $(".question-content").contents().find(".sv-form__actions");
     if (qtvalue.length) {
-      $("#siyavulaQContainer").contents().find(".sv-form__actions").remove();
+      $(".question-content").contents().find(".sv-form__actions").remove();
     }
 
-    $("#siyavulaQContainer").height(height + 40);
+    $(".question-content").height(height + 40);
 
     if (
-      $("#siyavulaQContainer").contents().find(".feedback--incorrect").length >
-      0
+      $(".question-content").contents().find(".feedback--incorrect").length > 0
     ) {
       $('[for="' + chkfalse.id + '"]').click();
     }
     if (
-      $("#siyavulaQContainer").contents().find(".feedback--correct").length > 0
+      $(".question-content").contents().find(".feedback--correct").length > 0
     ) {
       $('[for="' + chktrue.id + '"]').click();
     }
