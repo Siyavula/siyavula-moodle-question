47c82bba55fc4a704200b9ea260b69477f261ec1
Lehan <haaslewer2@gmail.com>
2022-05-19 15:20:28 +0200

    Refactor event handler

--- amd/src/external.js
+++ amd/src/external.js
@@ -19,52 +19,44 @@ define(["jquery", "core/ajax"], function ($, Ajax) {
         var nodes = Y.all(".latex-math");
         Y.fire(M.core.event.FILTER_CONTENT_UPDATED, { nodes: nodes });
 
-        $(".question-content").on("click", function (e) {
-          const responseId = e.currentTarget.dataset.response;
-          const activityId = e.currentTarget.id;
+        $(document).on("click", ".check-answer-button", function (e) {
+          e.preventDefault();
 
-          if (
-            e.target.className ===
-            "sv-button sv-button--primary check-answer-button"
-          ) {
-            e.preventDefault();
+          // Get all Siyavula inputs that have not been marked "readonly"
+          var formData = $(".response-query-input")
+            .not('[name*="|readonly"]')
+            .serialize();
 
-            // Get all Siyavula inputs that have not been marked "readonly"
-            var formData = $(".response-query-input")
-              .not('[name*="|readonly"]')
-              .serialize();
-
-            var submitresponse = Ajax.call([
-              {
-                methodname: "filter_siyavula_submit_answers_siyavula",
-                args: {
-                  baseurl: baseUrl,
-                  token: token,
-                  external_token: externalToken,
-                  activityid: activityId,
-                  responseid: responseId,
-                  data: formData,
-                },
+          var submitresponse = Ajax.call([
+            {
+              methodname: "filter_siyavula_submit_answers_siyavula",
+              args: {
+                baseurl: baseUrl,
+                token: token,
+                external_token: externalToken,
+                activityid: activityId,
+                responseid: responseId,
+                data: formData,
               },
-            ]);
+            },
+          ]);
 
-            submitresponse[0]
-              .done(function (response) {
-                var responseData = JSON.parse(response.response);
-                var html = responseData.response.question_html;
-                // Replace question HTML with marked HTML returned from the API
-                $(".question-content").html(html);
+          submitresponse[0]
+            .done(function (response) {
+              var responseData = JSON.parse(response.response);
+              var html = responseData.response.question_html;
+              // Replace question HTML with marked HTML returned from the API
+              $(".question-content").html(html);
 
-                // Hide nav buttons (Try exercise again/Go to next exercise)
-                $("#nav-buttons").css("display", "none");
+              // Hide nav buttons (Try exercise again/Go to next exercise)
+              $("#nav-buttons").css("display", "none");
 
-                // Typeset new HTML content
-                MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
-              })
-              .fail(function (ex) {
-                console.log(ex);
-              });
-          }
+              // Typeset new HTML content
+              MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+            })
+            .fail(function (ex) {
+              console.log(ex);
+            });
         });
       });
 
