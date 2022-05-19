1d94965e2ef27e64773d5db1031e5f8a5f74554f
Lehan <haaslewer2@gmail.com>
2022-05-19 13:08:01 +0200

    Refactor show/hide solution

--- amd/src/external.js
+++ amd/src/external.js
@@ -12,6 +12,9 @@ define(["jquery", "core/ajax"], function ($, Ajax) {
       $siyavula_activity_id
     ) {
       $(document).ready(function () {
+        // Expose showHideSolution to global window object
+        window.show_hide_solution = showHideSolution;
+
         // Initialise MathJax typesetting
         var nodes = Y.all(".latex-math");
         Y.fire(M.core.event.FILTER_CONTENT_UPDATED, { nodes: nodes });
@@ -49,26 +52,13 @@ define(["jquery", "core/ajax"], function ($, Ajax) {
               .done(function (response) {
                 var responseData = JSON.parse(response.response);
                 var html = responseData.response.question_html;
-                // Replace question HTML with marked HTML
+                // Replace question HTML with marked HTML returned from the API
                 $(".question-content").html(html);
 
                 const labelSolution = $(
                   ".question-content #show-hide-solution"
                 )[0];
                 const key = 0; // Because in quiz is only one response solution
-                labelSolution.innerHTML = "";
-
-                const newShowSpan = document.createElement("input");
-                newShowSpan.classList.add("sv-button");
-                newShowSpan.value = "Show the full solution";
-                newShowSpan.type = "button";
-                newShowSpan.id = `show${key}`;
-
-                const newHideSpan = document.createElement("input");
-                newHideSpan.value = "Hide the full solution";
-                newHideSpan.classList.add("sv-button");
-                newHideSpan.type = "button";
-                newHideSpan.id = `hide${key}`;
 
                 var is_correct = true;
                 const rsElement = labelSolution.nextSibling; // Response information
@@ -80,36 +70,10 @@ define(["jquery", "core/ajax"], function ($, Ajax) {
                   is_correct = false;
                 }
 
-                if (is_correct == false) {
-                  newShowSpan.style.display = "none";
-                } else {
-                  newHideSpan.style.display = "none";
-                }
-
-                labelSolution.append(newShowSpan);
-                labelSolution.append(newHideSpan);
-
                 // Hide nav buttons (Try exercise again/Go to next exercise)
                 $("#nav-buttons").css("display", "none");
 
-                const spanShow = labelSolution.querySelector(`#show${key}`);
-                const spanHide = labelSolution.querySelector(`#hide${key}`);
-                const functionClickSolution = (btnE) => {
-                  const currentSpan = btnE.target;
-                  if (currentSpan.value.includes("Show")) {
-                    spanShow.style.display = "none";
-                    spanHide.style.display = "initial";
-                  } else {
-                    spanShow.style.display = "initial";
-                    spanHide.style.display = "none";
-                  }
-
-                  // Animate show/hide solution
-                  $(`.${identificador}`).slideToggle();
-                };
-                spanShow.addEventListener("click", functionClickSolution);
-                spanHide.addEventListener("click", functionClickSolution);
-
+                // Typeset new HTML content
                 MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
               })
               .fail(function (ex) {
@@ -118,6 +82,18 @@ define(["jquery", "core/ajax"], function ($, Ajax) {
           }
         });
       });
+
+      function showHideSolution(button) {
+        const $button = jQuery(button);
+
+        // Toggle solution visibility
+        $button.parent().next().slideToggle("slow");
+
+        // Toggle button text
+        const previousValue = $button.attr("value");
+        const nextValue = $button.attr("data-alt-value");
+        $button.attr("value", nextValue).attr("data-alt-value", previousValue);
+      }
     },
   };
 });
