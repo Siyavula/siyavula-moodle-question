802909503e3a89a8df6add37fcf9a8234cd0c7b4
Lehan <haaslewer2@gmail.com>
2022-06-09 11:33:21 +0200

    Remove old js

--- amd/src/siyavulaqt.js
+++ amd/src/siyavulaqt.js
@@ -1,32 +1,38 @@
-export const init = (chktrue, chkfalse) => {
-  var timer = window.setInterval(() => {
-    var height = $(".question-content")
-      .contents()
-      .find(".sv-region-main")
-      .height();
+define("qtype_siyavulaqt/siyavulaqt", ["exports"], function (a) {
+  "use strict";
+  Object.defineProperty(a, "__esModule", { value: !0 });
+  a.init = void 0;
+  a.init = function init(chktrue, chkfalse) {
+    window.setInterval(function () {
+      var height = $(".question-content")
+        .contents()
+        .find(".sv-region-main")
+        .height();
 
-    var qtvalue = $(".question-content").contents().find(".sv-form__actions");
-    if (qtvalue.length) {
-      $(".question-content").contents().find(".sv-form__actions").remove();
-    }
+      var qtvalue = $(".question-content").contents().find(".sv-form__actions");
+      if (qtvalue.length) {
+        $(".question-content").contents().find(".sv-form__actions").remove();
+      }
 
-    $(".question-content").height(height + 40);
+      $(".question-content").height(height + 40);
 
-    if (
-      $(".question-content").contents().find(".feedback--incorrect").length > 0
-    ) {
-      $('[for="' + chkfalse.id + '"]').click();
-    }
-    if (
-      $(".question-content").contents().find(".feedback--correct").length > 0
-    ) {
-      $('[for="' + chktrue.id + '"]').click();
-    }
-    if (
-      $(".question-content").contents().find(".feedback--partly-correct")
-        .length > 0
-    ) {
-      $('[for="' + chktrue.id + '"]').click();
-    }
-  }, 1000);
-};
+      if (
+        $(".question-content").contents().find(".feedback--incorrect").length >
+        0
+      ) {
+        $("[id='" + chkfalse.id + "']").prop("checked", "checked");
+      }
+      if (
+        $(".question-content").contents().find(".feedback--correct").length > 0
+      ) {
+        $("[id='" + chktrue.id + "']").prop("checked", "checked");
+      }
+      if (
+        $(".question-content").contents().find(".feedback--partly-correct")
+          .length > 0
+      ) {
+        $("[id='" + chktrue.id + "']").prop("checked", "checked");
+      }
+    }, 1000);
+  };
+});
