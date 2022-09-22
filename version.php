12fe94ece3ec63fb25fced5fc7385e14835e0e43
Marli Coetzee <mareliefies@gmail.com>
2022-09-22 10:53:39 +0200

    Updating version nr

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_siyavulaqt';
-$plugin->version   = 2022071400;
+$plugin->version   = 2022092200;
 $plugin->requires  = 2019111809;
 
 $plugin->maturity  = MATURITY_STABLE;
